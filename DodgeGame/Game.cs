138f9cd2bf462837b27f5fba48a5df2cfbd9bc25
shlime <slimee@gmail.hu>
2016-09-17 23:54:24 +0200

    Movement implementation
    - Added the movement of the player.
    - Fixed an issue where the player could blink through the screen without
    crashing.

--- DodgeGame/Game.cs
+++ DodgeGame/Game.cs
@@ -4,38 +4,31 @@ namespace DodgeGame
 {
 	public class Game
 	{
-		Unit playerUnit = new Unit(1, 1, "::||::");
 
-		public bool Restart = false;
+		public Game()
+		{
+			playerUnit = new PlayerUnit(1, 1, "P");
+
+		}
+
+		private Unit playerUnit;
 
 		public void Run()
 		{
-			playerUnit.Draw();
-		}
 
-		public bool Update()
-		{
-			
-			ConsoleKeyInfo getKey = Console.ReadKey();
-
-			switch (getKey.Key)
+			while (true)
 			{
-				case ConsoleKey.UpArrow:
-					playerUnit.moveTo(playerUnit.X,playerUnit.Y - 1);
-					break;
-				case ConsoleKey.DownArrow:
-					playerUnit.moveTo(playerUnit.X, playerUnit.Y + 1);
-					break;
-				case ConsoleKey.RightArrow:
-					playerUnit.moveTo(playerUnit.X + 1, playerUnit.Y);
-					break;
-				case ConsoleKey.LeftArrow:
-					playerUnit.moveTo(playerUnit.X - 1, playerUnit.Y);
-					break;	
+				
+				playerUnit.Update();
+				playerUnit.Draw();
+				//Thread.Sleep(10);
 			}
+			 
 
-			return true;
+
 		}
+
+
 
 	}
 }
---
+++ DodgeGame/PlayerUnit.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DodgeGame
+{
+	public class PlayerUnit : Unit
+	{
+		public PlayerUnit(int x, int y, string UnitGraphic) : base(x,y,UnitGraphic)
+		{
+			
+		}
+
+		override public void Update()
+		{
+			
+
+			if (Console.KeyAvailable == true)
+			{
+
+				ConsoleKeyInfo getKey = Console.ReadKey(true);
+				switch (getKey.Key)
+				{
+					case ConsoleKey.UpArrow:
+					case ConsoleKey.W:
+						Y = Y - 1;
+						break;
+					case ConsoleKey.DownArrow:
+					case ConsoleKey.S:
+						Y = Y + 1;
+						break;
+					case ConsoleKey.RightArrow:
+					case ConsoleKey.D:
+						X = X+1;
+						break;
+					case ConsoleKey.LeftArrow:
+					case ConsoleKey.A:
+						X = X-1;
+						break;
+				}
+			}
+
+
+			base.Update();
+		}
+	}
+}
+
